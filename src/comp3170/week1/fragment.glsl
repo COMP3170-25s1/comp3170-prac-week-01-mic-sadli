a33a649ffe2112766cd2314edf91df612ac798d4
Michael Sadli <126532262+mic-sadli@users.noreply.github.com>
2025-03-03 16:38:10 +1100

    Diamond created!
    Changed formula to create diamond shape

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -1,16 +1,16 @@
 #version 410
 
-uniform vec3 u_colour;			// colour as a 3D vector (r,g,b)
+uniform vec3 u_colour;	// colour as a 3D vector (r,g,b)
 uniform vec2 u_screenSize;  	// screen dimensions in pixels
 
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   vec2 v = abs(p - vec2(0.25,0.5);
-float d = max(v.x, v.y)/0.5;     
-  
-   if (d < 0.5) {
+vec2 p = gl_FragCoord.xy / u_screenSize; // scale p into range (0,0) to (1,1)
+vec2 v = abs(p - vec2(0.5, 0.5)); 
+float d = v.x + v.y;  
+
+   if (d < 0.25) {
       o_colour = vec4(u_colour, 1);
    }
    else {
